e9367e376dbbc3c1de3801ec774f29bcb7ada36f
TITAN <aluochmarion7@gmail.com>
2024-03-20 14:54:02 +0300

    complete scope lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName ='bob'
+function upperCaseCustomerName() {
+    if (typeof window !== 'undefined' && customerName) {
+      customerName = customerName.toUpperCase();
+    }
+  }
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+  }
+  const leastFavoriteCustomer = 'hated';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'loved'}
